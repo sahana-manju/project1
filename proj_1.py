e60446ab44e010ff0375d6036ab2965718bbff6e
sahana-manju <56404564+sahana-manju@users.noreply.github.com>
2023-11-03 18:53:54 -0400

    Update proj_1.py

--- proj_1.py
+++ proj_1.py
@@ -1,3 +1,4 @@
+#Importing necessary libraries
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -6,15 +7,12 @@ import pandas as pd
 import plotly.express as px
 import hiplot as hip
 from PIL import Image
-from streamlit import runtime
-#from streamlit_option_menu import option_menu
-runtime.exists()
 
-
-
+#Setting the page title and page layout
 st.set_page_config(page_title="Air Quality Analysis", layout="wide")
 
-# CSS styles
+
+#Setting background image
 bg_img = '''
 <style>
 [data-testid="stAppViewContainer"] {
@@ -24,73 +22,60 @@ background-repeat: no-repeat;
 }
 </style>
 '''
-#https://images.unsplash.com/photo-1528459801416-a9e53bbf4e17
-#https://www.gettyimages.ca/detail/photo/splashed-with-fresh-air-royalty-free-image/1127069296?adppopup=true
-#WQD6TCLOozg
 st.markdown(bg_img, unsafe_allow_html=True)
 
-#selection = option_menu(None, ["Home", "Check Global Statistics", "Check Air Quality for a region"], 
-    #icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    #menu_icon="cast", 
- #   default_index=0, orientation="horizontal")
-#st.title('Air Quality Analysis')
+#Set Page Header and caption
 st.markdown(f'<h1 style="color:#336BFF;font-size:45px;">{"BreezoScan"}</h1>', unsafe_allow_html=True)
 st.markdown('**Breathing Innovation: Empowering Lives with Precision Air Quality Analysis.**')
-#st.markdown('**Breathing Innovation: Empowering Lives with Precision Air Quality Analysis.**')
 
-
-
-df_air=pd.read_csv('AQI and Lat Long of Countries.csv')
+#Imputation,EDA and scaling are explained in AQI_Imputation_and_EDA_code.pynb file in github repository
+#Loading the imputed dataset
+df_air=pd.read_csv('final_imputed_aqi_data.csv')
+#fetching unique countries values
 countries=list(df_air['Country'].unique())
 
+#Go back to Home Page button in the side bar
 if st.sidebar.markdown(f'<a href="https://aqiproject-dcbypjgtfn7bexqbfcnbin.streamlit.app"><button>Go back to Home Page</button> </a>', unsafe_allow_html=True):
     pass
 
 
+#Set dropdown options in the side bar
+selection = st.sidebar.selectbox("Choose your operation", ["Check Global Statistics", "Check Air Quality for desired region","Learn about Air Quality Index(AQI)","Summary Report by Researcher"])
 
-selection = st.sidebar.selectbox("Choose your operation", ["Check Global Statistics", "Check Air Quality for desired region","Learn about Air Quality Index(AQI)","Air Quality Prediction"])
 
-
-#, "Check Air Quality for desired region","Learn about Air Quality Index(AQI)","Air Quality Prediction"))
-
-#radio(
-
- #   "Choose your operation",
-
-  #  ("About","Check Global Statistics", "Check Air Quality for desired region","Learn about Air Quality Index(AQI)","Air Quality Prediction"))
-
+#Learn about AQI section creation
 if selection=='Learn about Air Quality Index(AQI)':
-#if st.sidebar.button("About",key="default_button_key"):
     st.markdown('**This page provides information to the user on Air Quality Index (AQI), a significant parameter in measuring the air quality**')
     image = Image.open('girl-transformed.jpg')
     st.image(image, caption='Breathing Innovation: Empowering Lives with Precision Air Quality Analysis.')
-    st.markdown("The Air Quality Index (AQI) is a numerical scale used to communicate how polluted the air currently is or how polluted it is forecast to become. It quantifies the concentration of specific air pollutants into a single number, which can help people understand the potential health impacts of breathing the air in a particular area. The pollutants commonly measured to calculate the AQI include:")
+    st.markdown("**The Air Quality Index (AQI) is a numerical scale used to communicate how polluted the air currently is or how polluted it is forecast to become. It quantifies the concentration of specific air pollutants into a single number, which can help people understand the potential health impacts of breathing the air in a particular area. The pollutants commonly measured to calculate the AQI include:**")
     bullet_points = [
-    "Ground-level ozone (O3)",
-    "Particulate matter (PM2.5 and PM10)",
-    "Carbon monoxide (CO)",
-    "Nitrogen dioxide (NO2)"
+    "**Ground-level ozone (O3)**",
+    "**Particulate matter (PM2.5 and PM10)**",
+    "**Carbon monoxide (CO)**",
+    "**Nitrogen dioxide (NO2)**"
     ]
     bullet_points_formatted = "\n".join([f"- {item}" for item in bullet_points])
     st.markdown(bullet_points_formatted)
-    st.markdown("The AQI scale typically ranges from 0 to 500, with lower values indicating better air quality and higher values indicating worse air quality. The scale is divided into different categories, each corresponding to a range of AQI values and indicating a different level of health concern. These categories are:")
+    st.markdown("**The AQI scale typically ranges from 0 to 500, with lower values indicating better air quality and higher values indicating worse air quality. The scale is divided into different categories, each corresponding to a range of AQI values and indicating a different level of health concern. These categories are:**")
     bullet_points2 = [
-        "0-50 (Good): Air quality is considered satisfactory, and air pollution poses little or no risk.",
-        "51-100 (Moderate): Air quality is acceptable; however, some pollutants may be a concern for a small number of individuals who are sensitive to air pollution.",
-        "101-150 (Unhealthy for sensitive groups): Members of sensitive groups may experience health effects, but the general public is less likely to be affected.",
-        "151-200 (Unhealthy): Everyone may begin to experience adverse health effects, and members of sensitive groups may experience more serious effects.",
-        "201-300 (Very Unhealthy): Health alert! Everyone may experience more serious health effects.",
-        "301-500 (Hazardous): Health warnings of emergency conditions; the entire population is more likely to be affected."
+        "**0-50 (Good): Air quality is considered satisfactory, and air pollution poses little or no risk.**",
+        "**51-100 (Moderate): Air quality is acceptable; however, some pollutants may be a concern for a small number of individuals who are sensitive to air pollution.**",
+        "**101-150 (Unhealthy for sensitive groups): Members of sensitive groups may experience health effects, but the general public is less likely to be affected.**",
+        "**151-200 (Unhealthy): Everyone may begin to experience adverse health effects, and members of sensitive groups may experience more serious effects.**",
+        "**201-300 (Very Unhealthy): Health alert! Everyone may experience more serious health effects.**",
+        "**301-500 (Hazardous): Health warnings of emergency conditions; the entire population is more likely to be affected.**"
     ]
     bullet_points_formatted2 = "\n".join([f"- {item}" for item in bullet_points2])
     st.markdown(bullet_points_formatted2)
-    st.markdown("It's important to note that different countries might have their own AQI scales with slight variations in the pollutants measured and the corresponding health categories. The AQI is often reported by weather agencies and environmental organizations and is widely used to inform the public about air quality conditions in their area. People can use this information to make decisions to protect their health, such as reducing outdoor activities on days with poor air quality, especially for individuals with respiratory or heart conditions, children, and the elderly.")
+    st.markdown("**It's important to note that different countries might have their own AQI scales with slight variations in the pollutants measured and the corresponding health categories. The AQI is often reported by weather agencies and environmental organizations and is widely used to inform the public about air quality conditions in their area. People can use this information to make decisions to protect their health, such as reducing outdoor activities on days with poor air quality, especially for individuals with respiratory or heart conditions, children, and the elderly.**")
 
+#Check Global Statistics section creation
 if selection=='Check Global Statistics':
-#if st.sidebar.button("Check Global Statistics"): 
     st.markdown("**This page depicts a variety of visualizations to display the Air Quality (Index) distribution across the entire globe. Furthermore, the distribution of airborne pollutants such as Ozone, dust particles, carbon monoxide, and nitrogen dioxide is also shown for the purpose of analyzing air pollution factors.**")
     col1, col2 = st.columns([1, 1])
     
+    #Bar Graph for countries with best and worst AQI
     with col1:
         st.markdown("### Countries with Worst air quality")
         df_air1=df_air[['Country','City','AQI Value','AQI Category']]
@@ -106,16 +91,8 @@ if selection=='Check Global Statistics':
         good = df_good_aqi.groupby('Country',as_index=False)['AQI Value'].mean().sort_values(by='AQI Value',ascending=False).head(10)
         plt.figure(figsize=(10,6))
         st.pyplot(sns.barplot(x='AQI Value', y='Country',data=good).figure)
-       
-
-
-
-
-    plt.figure(figsize=(10,6))
-    sns.barplot(x='AQI Value', y='Country',data=good)
     
     #Pie Chart 
-    #with col1:
     st.markdown("### Proportion of Air Quality spread across each country")
     column1 = st.selectbox("Choose Country", countries)
     df_country=df_air[df_air['Country']==column1]
@@ -123,15 +100,11 @@ if selection=='Check Global Statistics':
     fig=px.pie(values=df_country['AQI Value'], names=df_country['AQI Category'])
     st.plotly_chart(fig, theme="streamlit")
 
-    #Air Quality Spread across the globes
+    #Map distribution for Air Quality Spread across the globe
     plt.figure(figsize=(16, 8))
     st.markdown("### Global distribution of airborne contaminants and the Air Quality Index")
-    #pollutants=['Air Quality','Carbon Monoxide','Nitrogen dioxide','Ozone','Dust Particles']
-    #column1 = st.selectbox("Choose Pollutant", pollutants)
     column1 = st.radio(
-
     "Select the distribution of the characteristics that you wish to see worldwide.",
-
     ('Air Quality Index','Carbon Monoxide','Nitrogen dioxide','Ozone','Dust Particles'))
     pollutant_name='AQI Category'
     if column1=='Carbon Monoxide':
@@ -144,8 +117,7 @@ if selection=='Check Global Statistics':
         pollutant_name='PM2.5 AQI Category'
     st.pyplot(sns.scatterplot(data=df_air,x=df_air['lng'],y= df_air['lat'], hue=df_air[pollutant_name]).figure)
 
-
-    #with col2:
+    #Scatterplot for Relationship between pollutants and air quality
     st.markdown("### Relationship between pollutants and air quality")
     pollutants=['Carbon Monoxide','Nitrogen dioxide','Ozone','Dust Particles']
     column1 = st.selectbox("Choose Pollutant", pollutants)
@@ -164,6 +136,7 @@ if selection=='Check Global Statistics':
                 hover_data=['AQI Value'])
     st.plotly_chart(fig, theme="streamlit")
 
+    #HiPlot creation
     st.markdown("### In depth relationship between pollutants and Air Quality Index")
     st.markdown("Hover over each row in the following table to see the relationship between all the pollutants and Air Quality Index")
     print(f"HiPlot=={hip.__version__}")
@@ -172,9 +145,8 @@ if selection=='Check Global Statistics':
     exp_html=exp.to_html()
     st.components.v1.html(exp_html,height=1400)
     
-
+#Check Air Quality for desired region section
 if selection=='Check Air Quality for desired region':
-#if st.sidebar.button("Check Air Quality for desired region"): 
     st.markdown("**This page allows the user to examine the quality of the air in a particular region by selecting the country and city of their choice. To view the safe Air Quality Index (AQI) index levels, please refer to the AQI table below.**")
     image = Image.open('aqi_table2.jpg')
     st.image(image, caption='Breathing Innovation: Empowering Lives with Precision Air Quality Analysis.')
@@ -184,14 +156,11 @@ if selection=='Check Air Quality for desired region':
     column2 = st.selectbox("Choose City", cities)
     df_aqi=df_air[(df_air['Country']==column1)& (df_air['City']==column2)]
     quality=list(df_aqi['AQI Category'])
-    #st.markdown(f'<h1 style="color:#336BFF;font-size:34px;">{"Your Air Quality is : "}</h1>', unsafe_allow_html=True)
     class meter():
         def draw_meter(self,a,b):
             data_per = [50,50,50,50,50,50]
-            #explode = (0, 0, 0.3, 0, 0, 0)
             categories=['Good','Moderate','Unhealthy for Sensitive Groups','Unhealthy','Very Unhealthy','Hazardous']
             plt.pie(data_per,  labels = categories,colors=['green','yellow','orange','red','purple','brown'])
-            #plt.legend(categories, loc='upper left')
             circle = plt.Circle( (0,0), 0.7, color='white')
             arrow=plt.arrow(0, -0.05, a, b, 
                     head_width = 0.2, 
@@ -205,8 +174,6 @@ if selection=='Check Air Quality for desired region':
         st.markdown(f'<h1 style="color:#336BFF;font-size:34px;">{"Good"}</h1>', unsafe_allow_html=True)
         m=meter()
         meter_placeholder.pyplot(m.draw_meter(0.35,0.23))
-
-
     elif quality[0]=='Moderate':
         st.markdown(f'<h1 style="color:#336BFF;font-size:34px;">{"Air Quality is : Moderate"}</h1>', unsafe_allow_html=True)
         m=meter()
@@ -231,17 +198,20 @@ if selection=='Check Air Quality for desired region':
         st.markdown(f'<h1 style="color:#336BFF;font-size:34px;">{"Hazardous"}</h1>', unsafe_allow_html=True)
         m=meter()
         meter_placeholder.pyplot(m.draw_meter(0.35,-0.19))
+    else:
+        st.markdown(f'<h1 style="color:#336BFF;font-size:34px;">{"Sorry, Data is not available for this region"}</h1>', unsafe_allow_html=True)
+        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Summary Report by Researcher Section creation
+if selection=='Summary Report by Researcher':
+    st.markdown("**In our report summary, we present an analysis of the Air Quality Index (AQI) data collected on October 3rd, 2023, from various countries using our Air Sampling equipment. The findings indicate alarming trends in air quality across the globe.**")
+    bullet_points = [
+    "**Notably, cities in Russia and the USA are experiencing the most severe air pollution, falling under the 'Hazardous' category, while cities in Cyprus and Cameroon boast the best air quality standards.**",
+    "**A concerning pattern emerges in populous countries like India and China, where cities with large populations and extensive manufacturing plants exhibit extremely poor AQI levels.**",
+    "**Geographically, Asia demonstrates the poorest AQI ratings, contrasting with the Pacific region, which boasts the best AQI ratings.**",
+    "**Our visual analysis of AQI categories reveals a direct correlation between Dust Particles and the Air Quality Index, indicating a linear relationship. Conversely, understanding the connection between CO and NO2 pollutants proves challenging when their levels are below 50. However, once these levels surpass 50, they are classified as 'Unhealthy' or 'Hazardous.'**",
+    "**Particularly alarming is the prevalence of PM 2.5 and Ozone pollutants, both exhibiting high AQI values and falling within the 'Unhealthy' to 'Hazardous' range. These pollutants pose a significant threat to human health, especially impacting the respiratory system.**",
+    "**On a slightly positive note, the spread of Carbon Monoxide and Nitrogen Dioxide pollutants, while present, does not pose a significant hazard globally. In contrast, Dust Particles and Ozone are widespread and pose a substantial threat to Asian countries, exacerbating their air quality challenges.**"
+    ]
+    bullet_points_formatted = "\n".join([f"- {item}" for item in bullet_points])
+    st.markdown(bullet_points_formatted)
